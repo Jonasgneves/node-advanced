e49367cc96dd18e0f1f157b14c3d92144b61f4e9
Jonas Silva <jonas.silva@microcis.com.br>
2022-06-30 19:59:39 -0300

    ensure service calls LoadUserLoginApi once

--- tests/data/services/login-authentication.spec.ts
+++ tests/data/services/login-authentication.spec.ts
@@ -6,9 +6,11 @@ import { LoginAuthenticationService } from '@/data/services'
 export class LoadUserLoginApiSpy implements LoadUserLoginApi {
   signin?: LoginAuthentication.Params
   result = undefined
+  callsCount = 0
 
   async loadUserLoginApi (params: LoginAuthentication.Params): Promise<LoadUserLoginApi.Result> {
     this.signin = params
+    this.callsCount++
     return this.result
   }
 }
@@ -21,6 +23,7 @@ describe('LoginAuthenticationService', () => {
     await sut.perform({ user: 'any_login', password: 'any_senha' })
 
     expect(loadLoginUser.signin).toEqual({ user: 'any_login', password: 'any_senha' })
+    expect(loadLoginUser.callsCount).toBe(1)
   })
 
   it('Should return AuthenticationError when LoadUserLoginApi returns undefined', async () => {
