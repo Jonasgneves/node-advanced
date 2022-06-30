ffe3d1946e34d2473d0d1180dcd266fd77a65fd1
Jonas Silva <jonas.silva@microcis.com.br>
2022-06-30 20:07:40 -0300

    change test strategy to use jest mock

--- tests/data/services/login-authentication.spec.ts
+++ tests/data/services/login-authentication.spec.ts
@@ -1,34 +1,24 @@
 import { AuthenticationError } from '@/domain/errors'
-import { LoginAuthentication } from '@/domain/features'
-import { LoadUserLoginApi } from '@/data/contracts/apis'
 import { LoginAuthenticationService } from '@/data/services'
-
-export class LoadUserLoginApiSpy implements LoadUserLoginApi {
-  signin?: LoginAuthentication.Params
-  result = undefined
-  callsCount = 0
-
-  async loadUserLoginApi (params: LoginAuthentication.Params): Promise<LoadUserLoginApi.Result> {
-    this.signin = params
-    this.callsCount++
-    return this.result
-  }
-}
 
 describe('LoginAuthenticationService', () => {
   it('Should call LoadUserLoginApi with correct params', async () => {
-    const loadLoginUser = new LoadUserLoginApiSpy()
+    const loadLoginUser = {
+      loadUserLoginApi: jest.fn()
+    }
     const sut = new LoginAuthenticationService(loadLoginUser)
 
     await sut.perform({ user: 'any_login', password: 'any_senha' })
 
-    expect(loadLoginUser.signin).toEqual({ user: 'any_login', password: 'any_senha' })
-    expect(loadLoginUser.callsCount).toBe(1)
+    expect(loadLoginUser.loadUserLoginApi).toHaveBeenCalledWith({ user: 'any_login', password: 'any_senha' })
+    expect(loadLoginUser.loadUserLoginApi).toHaveBeenCalledTimes(1)
   })
 
   it('Should return AuthenticationError when LoadUserLoginApi returns undefined', async () => {
-    const loadLoginUser = new LoadUserLoginApiSpy()
-    loadLoginUser.result = undefined
+    const loadLoginUser = {
+      loadUserLoginApi: jest.fn()
+    }
+    loadLoginUser.loadUserLoginApi.mockResolvedValueOnce(undefined)
     const sut = new LoginAuthenticationService(loadLoginUser)
 
     const authResult = await sut.perform({ user: 'any_login', password: 'any_senha' })
