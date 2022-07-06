729603b9a9b21415cd7302fb01b7859e0d9d5132
Jonas Silva <jonas.silva@microcis.com.br>
2022-07-06 16:50:21 -0300

    ensure LoginController returns 400 if token is null

--- tests/application/controllers/login-controller.spec.ts
+++ tests/application/controllers/login-controller.spec.ts
@@ -23,4 +23,15 @@ describe('LoginController', () => {
       data: new Error('The field token is Required')
     })
   })
+
+  it('should return 400 if token is null', async () => {
+    const sut = new LoginController()
+
+    const httpResponse = await sut.handle({ token: null })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The field token is Required')
+    })
+  })
 })
