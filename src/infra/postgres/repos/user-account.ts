040590ce17c0c46dba6c24afbe864793763595fd
Jonas Silva <jonas.silva@microcis.com.br>
2022-07-15 00:07:20 -0300

    improve code quality

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -3,13 +3,11 @@ import { getRepository } from 'typeorm'
 import { User } from '@/infra/postgres/entities'
 
 export class PgUserAccountRepository implements UserRepository {
-  async loadUser (params: UserRepository.Params): Promise<UserRepository.Result> {
+  async loadUser ({ user }: UserRepository.Params): Promise<UserRepository.Result> {
     const pgUserRepo = getRepository(User)
-    const pgUser = await pgUserRepo.findOne({ name: params.user })
+    const pgUser = await pgUserRepo.findOne({ name: user })
     if (pgUser !== undefined) {
-      return {
-        userId: pgUser.id.toString()
-      }
+      return { userId: pgUser.id.toString() }
     }
   }
 }
