7b7e5c4a1b1951de167d9e6a24f759b1d1c5be7e
Jonas Silva <jonas.silva@microcis.com.br>
2022-07-26 11:35:51 -0300

    rename JwtTokenGenerator to JtwTokenHandler

--- src/main/factories/use-cases/login-authentication.ts
+++ src/main/factories/use-cases/login-authentication.ts
@@ -1,10 +1,10 @@
 import { setupLoginAuthentication, LoginAuthentication } from '@/domain/use-cases'
 import { makeUserAccountRepository } from '@/main/factories/repos'
-import { makeJwtTokenGenerator } from '@/main/factories/crypto'
+import { makeJwtTokenHandler } from '@/main/factories/crypto'
 
 export const makeLoginAuthentication = (): LoginAuthentication => {
   return setupLoginAuthentication(
     makeUserAccountRepository(),
-    makeJwtTokenGenerator()
+    makeJwtTokenHandler()
   )
 }
