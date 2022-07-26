a6507b9894fadc9175eb33ec075ea4d8dfc49e5d
Jonas Silva <jonas.silva@microcis.com.br>
2022-07-26 12:45:09 -0300

    improve code quality

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -9,14 +9,18 @@ type Model = Error | { userId: string }
 export class AuthenticationMiddleware {
   constructor (private readonly authorize: Authorize) {}
   async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
-    const error = new RequiredStringValidator(authorization, 'authorization').validate()
-    if (error !== undefined) return forbbiden()
+    if (!this.validate({ authorization })) return forbbiden()
     try {
       const userId = await this.authorize({ token: authorization })
       return ok({ userId })
     } catch {
       return forbbiden()
     }
+  }
+
+  private validate ({ authorization }: HttpRequest): boolean {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    return error === undefined
   }
 }
 
