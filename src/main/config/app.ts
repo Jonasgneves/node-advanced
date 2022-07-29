015f322cb22eed75236a150c7fc3d6016b9adcbb
Jonas Silva <jonas.silva@microcis.com.br>
2022-07-29 12:50:18 -0300

    ensure auth middleware returns 200 if authorization header is valid

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,5 +1,7 @@
 import { setupMiddlewares } from '@/main/config/middlewares'
 import { setupRoutes } from '@/main/config/routes'
+
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
